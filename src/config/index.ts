0f03f8cd473e1c3d5d35a66fda1a7ea73144017f
Lucas Acosta <lacosta@makingsense.com>
2018-09-11 16:59:25 -0300

    Remove unneeded dependencies

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,6 @@ config({ path: pathResolve(__dirname, `./env/.env.${env.NODE_ENV}`) });
 export default {
   environment: env.NODE_ENV,
   port: Number(env.PORT),
-  mongoConnectionString: env.MONGO_CONNECTION_STRING,
   SQL: {
     db: env.SQL_DB,
     username: env.SQL_USERNAME,
